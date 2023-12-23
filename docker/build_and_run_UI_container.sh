04e52c6571134412d6808490bab3655f7d27b8cf
C'tri Goudie <ctricoding@ctri.co.uk>
2023-12-23 11:10:58 +0000

    adjust sample build script

--- docker/build_and_run_UI_container.sh
+++ docker/build_and_run_UI_container.sh
@@ -1,5 +1,5 @@
-docker build -f .dockerfile -t straders_UI .
+docker build -f .dockerfile -t straders_ui .
 docker network create spacetraders-network
-docker stop straders_UI_instance
-docker rm straders_UI_instance
-docker run --name straders_UI_instance  --network spacetraders-network -e ST_DB_HOST=localhost -e ST_DB_PASS=mysecretpassword -e ST_DB_PORT=6432  -d straders_UI 
+docker stop straders_ui_instance
+docker rm straders_ui_instance
+docker run --name straders_ui_instance  --network spacetraders-network -p 80:3000 -e ST_DB_HOST=localhost -e ST_DB_PASS=mysecretpassword -e ST_DB_PORT=6432  -d straders_ui
